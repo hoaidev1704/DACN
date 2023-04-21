d04ad2fce8a12a2125ab7056b2c457706bccda42
Hoai Le <Hoai.Le@hansencx.com>
2023-04-21 14:35:00 +0700

    Update Package

---
+++ pds/src/test/java/nlu/danc/pds/PdsApplicationTests.java
@@ -0,0 +1,13 @@
+package nlu.danc.pds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
